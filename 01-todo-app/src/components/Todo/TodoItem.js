c1327a5354f07d993bcb8362fdb2f3039287bb52
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2022-04-01 09:27:07 +0700

    Todo apps

--- 01-todo-app/src/components/Todo/TodoItem.js
+++ 01-todo-app/src/components/Todo/TodoItem.js
@@ -3,14 +3,11 @@ import classes from "./TodoItem.module.css";
 export const TodoItem = (props) => {
   const { id, title, isActive, isCompleted } = props.item;
   return (
-    <li
-      className={classes["todo__item"]}
-      key={id}
-      onClick={props.onRemoveTodo.bind(this, id)}
-    >
-      <span>{isActive ? "Active" : "Inactive"}</span>
-      <span>{title}</span>
-      <span>{isCompleted ? "Complete" : "Incomplete"}</span>
+    <li className={classes["todo__item"]} key={id}>
+      <input type="checkbox" id="active" checked={isActive} />
+      <span className={classes.title}>{title}</span>
+      <input type="checkbox" id="complete" checked={isCompleted} />
+      <button onClick={props.onRemoveTodo.bind(this, id)}>Delete</button>
     </li>
   );
 };
